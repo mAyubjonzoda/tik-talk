4a51ae7e95b688df35bbb4666094051be7566b3b
Muhammad Ayubjonzoda <m.ayubjonzoda@gmail.com>
2024-12-08 12:32:10 +0500

    search-page is ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
-import { ProfileService } from './data/services/profile.service';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule, JsonPipe } from '@angular/common';
-import { Profile } from './data/interfaces/profile.interfaces';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCardComponent, CommonModule, JsonPipe],
+  imports: [RouterOutlet, ProfileCardComponent, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'title';
-  profileService = inject(ProfileService);
-  profiles: Profile[] = [];
-
-  constructor() {
-    this.profileService.getTestAccounts().subscribe((val: Profile[]) => {
-      this.profiles = val;
-    });
-  }
 }
--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProfileService } from '../../data/services/profile.service';
+import { Profile } from '../../data/interfaces/profile.interfaces';
+import { ProfileCardComponent } from '../../common-ui/profile-card/profile-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [ProfileCardComponent, CommonModule],
   templateUrl: './search-page.component.html',
-  styleUrl: './search-page.component.scss'
+  styleUrl: './search-page.component.scss',
 })
 export class SearchPageComponent {
+  profileService = inject(ProfileService);
+  profiles: Profile[] = [];
 
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val: Profile[]) => {
+      this.profiles = val;
+    });
+  }
 }
